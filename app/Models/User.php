c5cd103bdde0d7d1fac570901de7ec91622355c9
GaGiiiii <dragoslav.gagi8@gmail.com>
2021-11-01 13:51:16 +0100

    Created Models And Their Relationships

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,37 +8,54 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
-{
-    use HasApiTokens, HasFactory, Notifiable;
+class User extends Authenticatable {
+  use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var string[]
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var string[]
+   */
+  protected $fillable = [
+    'first_name',
+    'last_name',
+    'email',
+    'password',
+    'image',
+  ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+  /**
+   * The attributes that should be hidden for serialization.
+   *
+   * @var array
+   */
+  protected $hidden = [
+    'password',
+    'remember_token',
+  ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+  /**
+   * The attributes that should be cast.
+   *
+   * @var array
+   */
+  protected $casts = [
+    'email_verified_at' => 'datetime',
+  ];
+
+  public function comments() {
+    return $this->hasMany(Comment::class)->orderBy('created_at', 'DESC');
+  }
+
+  public function likes() {
+    return $this->hasMany(Like::class)->orderBy('created_at', 'DESC');
+  }
+
+  public function posts() {
+    return $this->hasMany(Post::class)->orderBy('created_at', 'DESC');
+  }
+
+  public function interests() {
+    return $this->hasMany(Interest::class);
+  }
 }
